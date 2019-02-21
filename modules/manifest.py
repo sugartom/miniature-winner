4bb483a7d88fe1dfcfae3da316385b15feb38cfe
Weiwu Pang <weiwupan@usc.edu>
2019-02-21 10:56:45 -0800

    protobuf file

---
+++ modules/manifest.py
@@ -0,0 +1,45 @@
+
+Text2Text = {
+    "input": TextProto,
+    "output": TextProto,
+    "input_metadata": XXX,
+    "output_metadata": XXX,
+    "computational_device": 1,
+    "library_code_path": "text2text.py"
+}
+
+
+Text2Speech = {
+    "input": TextProto,
+    "output": AudioProto,
+    "input_metadata": XXX,
+    "output_metadata": XXX,
+    "computational_device": 1,
+    "library_code_path": "text2speech.py"
+}
+
+
+Speech2Text = {
+    "input": AudioProto,
+    "output": TextProto,
+    "input_metadata": XXX,
+    "output_metadata": XXX,
+    "computational_device": 1,
+    "library_code_path": "text2speech.py"
+}
+
+TextEncoder = {
+    "input": TextProto,
+    "output": TextProto,
+    "input_metadata": XXX,
+    "output_metadata": XXX,
+    "computational_device": 0,
+}
+
+TextDecoder = {
+    "input": TextProto,
+    "output": TextProto,
+    "input_metadata": XXX,
+    "output_metadata": XXX,
+    "computational_device": 0,
+}
