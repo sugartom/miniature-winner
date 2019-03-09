7fd58d2fb9737cb1e9c2eff33681551b335cf187
Weiwu Pang <weiwupan@usc.edu>
2019-03-08 20:47:42 -0800

    change Jasper module to use tensorflow serving

--- modules/Jasper.py
+++ modules/Jasper.py
@@ -1,7 +1,10 @@
 import tensorflow as tf
-import sys
 from OpenSeq2Seq.open_seq2seq.utils.utils import deco_print, get_base_config, check_logdir,\
     create_logdir, create_model
+import grpc
+from tensorflow_serving.apis import predict_pb2
+from tensorflow_serving.apis import prediction_service_pb2_grpc
+from tensorflow.python.framework import tensor_util
 
 def get_model(args, scope):
     with tf.variable_scope(scope, reuse=tf.AUTO_REUSE):
@@ -22,22 +25,13 @@ class Jasper:
                     ]
         self.model, checkpoint_S2T = get_model(args_S2T, "S2T")
 
-        sess_config = tf.ConfigProto(allow_soft_placement=True)
-        sess_config.gpu_options.allow_growth = True
-        self.sess = tf.Session(config=sess_config)
-
-        vars_S2T = {}
-        for v in tf.get_collection(tf.GraphKeys.VARIABLES):
-            if "S2T" in v.name:
-                vars_S2T["/".join(v.op.name.split("/")[1:])] = v
-
-        saver_S2T = tf.train.Saver(vars_S2T)
-        saver_S2T.restore(self.sess, checkpoint_S2T)
-
         self.fetches = [
             self.model.get_data_layer().input_tensors,
             self.model.get_output_tensors(),
         ]
+
+        self.channel = grpc.insecure_channel('0.0.0.0:8500')
+        self.stub = prediction_service_pb2_grpc.PredictionServiceStub(self.channel)
 
     def PreProcess(self, input):
         feed_dict = self.model.get_data_layer().create_feed_dict(input)
@@ -45,7 +39,47 @@ class Jasper:
         return feed_dict
 
     def Apply(self, feed_dict):
-        inputs, outputs = self.sess.run(self.fetches, feed_dict=feed_dict)
+        audio = feed_dict[self.model.get_data_layer().input_tensors[
+            "source_tensors"][0]]
+        audio_length = feed_dict[self.model.get_data_layer().input_tensors[
+            "source_tensors"][1]]
+        x_id = feed_dict[self.model.get_data_layer().input_tensors[
+            "source_ids"][0]]
+
+
+        request = predict_pb2.PredictRequest()
+        request.model_spec.name = 'jasper'
+        request.model_spec.signature_name = 'predict_output'
+        request.inputs['audio'].CopyFrom(
+            tf.contrib.util.make_tensor_proto(audio, shape=list(audio.shape)))
+        request.inputs['audio_length'].CopyFrom(
+            tf.contrib.util.make_tensor_proto(audio_length,
+                                              shape=list(audio_length.shape)))
+        request.inputs['x_id'].CopyFrom(
+            tf.contrib.util.make_tensor_proto(x_id,
+                                              shape=list(x_id.shape)))
+
+        result_future = self.stub.Predict.future(request, 5.0)  # 5 seconds
+        exception = result_future.exception()
+        if exception:
+            print(exception)
+        else:
+            print('Result returned from rpc')
+
+        inputs = self.model.get_data_layer().input_tensors
+        indices_decoded_sequence = tensor_util.MakeNdarray(
+            result_future.result().outputs['indices_decoded_sequence'])
+        values_decoded_sequence = tensor_util.MakeNdarray(
+            result_future.result().outputs['values_decoded_sequence'])
+        dense_shape_decoded_sequence = tensor_util.MakeNdarray(
+            result_future.result().outputs['dense_shape_decoded_sequence'])
+
+        outputs = tf.SparseTensorValue(indices=indices_decoded_sequence,
+                                       values=values_decoded_sequence,
+                                       dense_shape=dense_shape_decoded_sequence)
+
+        outputs = [outputs]
+
 
         return inputs, outputs
 
